0d1218919a45ada94dbb7a05ebce388d01999008
omargallob <omargallob@gmail.com>
2009-11-19 19:26:41 +0000

    gmaps stage 1.1 all to newport

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -23,19 +23,19 @@ class PropertiesController < ApplicationController
     @page = Page.find_by_name('properties')
   @subpages = @page.subpages
        set_meta_tags :title =>  "("+@property.location.zipcod+") "+@property.location.region+" - "+ @property.title
+       results = Geocoding::get("Newport Beach")
 
        @map = GMap.new("map")
-           @map.control_init(:large_map => true,:map_type => true)
-           
+       @map.control_init(:large_map => true,:map_type => true)           
           # @map.overlay_init(GMarker.new([75.6,-42.467],:title => "Hello", :info_window => "Info! Info!"))
 
 
-           results = Geocoding::get("Newport Beach")
-            if results.status == Geocoding::GEO_SUCCESS
-              coord = results[0].latlon
-              @map.overlay_init(GMarker.new(coord,:info_window => "Newport Beach"))
-            end
-            @map.center_zoom_init(coord,20)
+
+        if results.status == Geocoding::GEO_SUCCESS
+          coord = results[0].latlon
+          @map.center_zoom_init(coord,15)
+        end
+      
   end 
   
   def feed
