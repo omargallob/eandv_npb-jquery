49141249a67fcef6a1550b7a660fa2d1b25844b3
omar gallo <omar@banus-dev.(none)>
2010-01-12 17:01:28 +0100

    backend - pages - services and properties linked from the pages section

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -2,9 +2,10 @@ class Admin::OverviewController < Admin::BaseController
  
   def index
     @pages = Page.find(:all)
-    @categories = Category.find_main
-    @countries = Country.find(:all)
+    @categories = Category.find(:all)
+		@countries = Country.find(:all)
     @facilities = Facility.find(:all)
     @properties = Property.find(:all)
+    @metatags = Metatag.find(:all)
   end
 end
