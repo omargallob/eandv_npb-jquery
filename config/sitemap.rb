bbc3010ac0c74d116ee8996b0e474ca8db29d187
omar <omar@MacBook-Pro-de-juancarlos.local>
2010-01-31 17:45:33 +0100

    removed sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,36 +1,23 @@
-# Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://npb.heroku.com"
+Sitemap::Routes.host = 'http://npb.heroku.com'
+Sitemap::Routes.priority = 0.8    # default is 1.0
 
-SitemapGenerator::Sitemap.add_links do |sitemap|
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: sitemap.add path, options
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly', 
-  #           :lastmod => Time.now, :host => default_host
+Sitemap::Routes.draw do |map|
+
+  # root routes
+  map.root :controller => 'viewer', :action => 'show', :name => "name"
+
+  #map.connect '/properties/:id', :controller => 'properties', :action => 'show', :substitution => {:model => 'Properties',:id => 'id'}
 
   
-  # Examples:
+  map.resources :properties
+  #map.services_par "/services ", :controller => "services", :action =>"index"
   
-  # add '/properties'
-  sitemap.add properties_path, :priority => 0.7, :changefreq => 'daily'
-
-  # add all individual properties
-  Property.find(:all).each do |a|
-    sitemap.add property_path(a), :lastmod => a.updated_at
-  end
+  map.connect 'services', :controller => "services", :action => "index"
+  map.connect 'services/:parent',  :controller => "services", :action => "show", :substitution => {:model => "Category",:category => "parent_id"} 
+  #map.services_sub 'services/:parent', :controller => 'sitemaps', :action => 'index'
+  #map.services '/services/:parent/:category', :controller => "services", :action => "show", :substitution => {:model => 'Category',:parent=>"parent_id",:category => 'name'}
   
-  sitemap.add services_path, :priority => 0.7, :changefreq => 'daily'
-
-  Category.find_main.each do |a|
-    sitemap.add services_par_path(:category => a.name), :lastmod => a.updated_at
-  end
-  
-  # add merchant path
-  #sitemap.add '/purchase', :priority => 0.7, :host => "https://www.example.com"
-  
+  #map.resources :pages
+  #map.connect 'services', :controller => "services", :action => "index"
+  #map.connect 'services/:parent', :controller => "services", :action => "show", :substitution => {:model => "Category", :parent => "name"} 
 end
