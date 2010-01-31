b15c677b06294cb2138b8e0b22b89e82de6a1fa5
omar <omar@MacBook-Pro-de-juancarlos.local>
2010-01-31 18:11:51 +0100

    sitemap_index

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "http://npb.heroku.com"
 
 SitemapGenerator::Sitemap.add_links do |sitemap|
   # Put links creation logic here.
