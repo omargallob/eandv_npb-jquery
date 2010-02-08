83d24c769e9255e14ca6b532d7c29b11d81b1dda
omar gallo <omar@banus-dev.(none)>
2010-02-08 02:58:16 +0100

    different bog views for idmnex and for show

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,11 +2,15 @@ class BlogController < ApplicationController
   def index
 	 @page = Page.find_by_name('blog')
    @posts = Post.find(:all)
+
+   set_meta_tags :title =>  "Blog "
+
   end
 
   def show
  		@page = Page.find_by_name('blog')
 	  @post = Post.find_by_title(params[:id])
+	  set_meta_tags :title =>  "Blog | " + @post.title
   end
 
 end
