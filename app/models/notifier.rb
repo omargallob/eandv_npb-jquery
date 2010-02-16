073b1714e792093be6f880eebc976c5ae75a895e
omar gallo <omar@banus-dev.(none)>
2010-02-16 12:04:03 +0100

    send email v1

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
  def signup_newsletter(contact)
    recipients contact.email
    bcc        ["Mail Watcher <watcher@engelandvoelkersnewportbeach.com>"]
-   from       "omargallob@gmail.com"
+   from       "system@engelandvoelkersnewportbeach.com"
    subject    "Welcome to the E&V Newsletter"
    body       :account => contact
  end
