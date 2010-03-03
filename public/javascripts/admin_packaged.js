504633e8cefc19949f08df28c5c30a439b4a829c
omar gallo <omar@banus-dev.(none)>
2010-03-03 18:00:37 +0100

    new backend menu

--- public/javascripts/admin_packaged.js
+++ public/javascripts/admin_packaged.js
@@ -548,4 +548,8 @@ Selection.refresh();};$hdl_holder.hide();interfaceUpdate(true);var api={animateT
 {var loadsrc=options.useImg||from.src;var img=new Image();img.onload=function(){$.Jcrop(from,options);};img.src=loadsrc;};if(typeof(options)!=='object')options={};this.each(function()
 {if($(this).data('Jcrop'))
 {if(options=='api')return $(this).data('Jcrop');else $(this).data('Jcrop').setOptions(options);}
-else attachWhenDone(this);});return this;};})(jQuery);
+else attachWhenDone(this);});return this;};})(jQuery);function addLoadEvent(func){var oldonload=window.onload;if(typeof window.onload!='function'){window.onload=func;}else{window.onload=function(){oldonload();func();}}}
+function prepareMenu(){if(!document.getElementsByTagName)return false;if(!document.getElementById)return false;if(!document.getElementById("menu"))return false;var menu=document.getElementById("menu");var root_li=menu.getElementsByTagName("li");for(var i=0;i<root_li.length;i++){var li=root_li[i];var child_ul=li.getElementsByTagName("ul");if(child_ul.length>=1){li.onmouseover=function(){if(!this.getElementsByTagName("ul"))return false;var ul=this.getElementsByTagName("ul");ul[0].style.display="block";return true;}
+li.onmouseout=function(){if(!this.getElementsByTagName("ul"))return false;var ul=this.getElementsByTagName("ul");ul[0].style.display="none";return true;}}}
+return true;}
+addLoadEvent(prepareMenu);
